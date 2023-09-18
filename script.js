9e71c558ec271a6eea3819e12d56b69a87ccedf0
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-09-18 10:37:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 function debounce(callback, delay, immediate = false) {
+    let timeoutId;
+    let lastArgs;
+    let lastThis;
+    let result;
   
+    const executeCallback = () => {
+      result = callback.apply(lastThis, lastArgs);
+      lastArgs = null;
+      lastThis = null;
+    };
+  
+    const debounced = function (...args) {
+      lastArgs = args;
+      lastThis = this;
+      if (immediate && !timeoutId) {
+        executeCallback();
+      }
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(executeCallback, delay);
+      return result;
+    };
+  
+    debounced.cancel = () => {
+      clearTimeout(timeoutId);
+      lastArgs = null;
+      lastThis = null;
+    };
+  
+    return debounced;
   }
   
   module.exports = debounce;
